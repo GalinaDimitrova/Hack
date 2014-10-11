477eb7eab8136bfc08cbef3d8eb6c23f5977d6da
GalinaDimitrova <galinadimitrova1993@gmail.com>
2014-10-12 01:26:49 +0300

    Delete file test.py

--- week0/1-upr/test.py
+++
@@ -1,7 +0,0 @@
-from copy import copy, deepcopy
-
-def matrix_bombing_plan(m):
-    new_matrix = [row[:] for row in m]
-    print(new_matrix)
-    
-matrix_bombing_plan([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
